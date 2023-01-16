0fa6e900adbea9a3f1a470194d14fb3bd4b677d8
tony tolver <andrademaresantonio00@gmail.com>
2023-01-16 00:24:42 -0600

    Pantalla de Crear Producto

--- CRUD_NetCore7_Razor/Pages/Productos/Crear.cshtml.cs
+++ CRUD_NetCore7_Razor/Pages/Productos/Crear.cshtml.cs
@@ -1,3 +1,5 @@
+using CRUD_NetCore7_Razor.Datos;
+using CRUD_NetCore7_Razor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +7,16 @@ namespace CRUD_NetCore7_Razor.Pages.Productos
 {
     public class CrearModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
+        public CrearModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Vincular el modelo
+        [BindProperty]
+        public Producto Producto { get; set; }
+
         public void OnGet()
         {
         }
